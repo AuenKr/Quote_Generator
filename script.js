f07b4fa26dc776b366d6523f65946d24b93f5282
Auen <auenkumar64@gmail.com>
2022-08-01 19:03:27 +0530

    Final Buid

--- script.js
+++ script.js
@@ -4,11 +4,26 @@ const apiUrl = "https://type.fit/api/quotes"
 let apiQuote = [];
 let quote = [];
 
+let content = document.getElementById("content")
 let quoteText = document.getElementById("quote");
 let quoteAuthor = document.getElementById("author");
 let tweetBtn = document.getElementById("share");
 let nextBtn = document.getElementById("next-quote");
+let loader = document.getElementById("loader");
 
+// loader 
+function loading() {
+    loader.hidden = false;
+    content.hidden = true;
+}
+
+// continue
+function After_Loading() {
+    if (!loader.hidden) {
+        loader.hidden = true;
+        content.hidden = false;
+    }
+}
 
 function NewQuote() {
     quote[0] = apiQuote[Math.floor(Math.random()*(apiQuote.length))];
@@ -20,11 +35,12 @@ function NewQuote() {
     else{
         quoteAuthor.textContent ="~ " + quote[0]["author"];
     }
-    
+    After_Loading();
 }
 
 async function GetQuote() {
     try {
+        loading();
         const respone = await fetch(apiUrl);
         apiQuote = await respone.json();
         NewQuote();
